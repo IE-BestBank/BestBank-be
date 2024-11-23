e20a59fac856eeb9f2c3d279a86b864631392bc6
Daniel Mora <danielmora@Daniels-MacBook-Air-2.local>
2024-11-23 18:57:14 +0100

    Test hardcode

--- app.py
+++ app.py
@@ -1,4 +1,20 @@
 from iebank_api import app
 
-if __name__ == '__main__':
+# Import the `azure.monitor.opentelemetry` package.
+from azure.monitor.opentelemetry import configure_azure_monitor
+
+# Import the tracing API from the `opentelemetry` package.
+from opentelemetry import trace
+import json
+
+# Load the connection string from appinsights.json
+with open("appinsights.json", "r") as f:
+    config = json.load(f)
+    connection_string = config["connectionString"]
+
+# Configure Azure Monitor with the loaded connection string
+configure_azure_monitor(connection_string=connection_string)
+
+# Start the Flask application
+if __name__ == "__main__":
     app.run(debug=True)
