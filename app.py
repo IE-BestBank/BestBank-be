9f708e3eb4c7aea2f92cc43a109de24b0970f271
Daniel Arturo Mora <88565349+daMS2005@users.noreply.github.com>
2024-12-06 21:27:02 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,12 @@
-#Making comment
 from iebank_api import app
+from applicationinsights.flask.ext import AppInsights
 import os
 
+# Initialize Application Insights
+app.config['APPINSIGHTS_INSTRUMENTATIONKEY'] = os.getenv('APPLICATIONINSIGHTS_CONNECTION_STRING', '')
+appinsights = AppInsights(app)
+
 if __name__ == '__main__':
+    # Determine debug mode from environment variables
     debug_mode = os.getenv('FLASK_DEBUG', 'False').lower() in ['true', '1', 't']
     app.run(debug=debug_mode)
